816a9ac77edac445b2c4452270108bf8bfee88c5
André Salgado <andrecostasalgadof@gmail.com>
2026-05-20 15:58:32 -0300

    feat: configuração base da api

--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,11 @@
- 
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'https://back-end-projeto-integrador.onrender.com',
+});
+
+api.interceptors.request.use((config) => {
+  return config;
+});
+
+export default api;
